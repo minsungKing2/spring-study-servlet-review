4fd7b0bb080345bea4f96378e7aa88d5b0527d5d
MinSung Jung <minpro22@naver.com>
2025-08-27 10:48:02 +0900

    25.08.27 서블릿 회원 관리 웹 애플리케이션 로직

---
+++ src/main/java/hello/servlet/web/servlet/MemberFormServlet.java
@@ -0,0 +1,44 @@
+package hello.servlet.web.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+//회원 등록 폼 - HTML Form
+
+/**
+ * MemberListServlet 은 다음 순서로 동작한다.
+ * 1. memberRepository.findAll() 을 통해 모든 회원을 조회한다.
+ * 2. 회원 목록 HTML 을 for 루프를 통해서 회원 수 만큼 동적으로 생성하고 응답한다.
+ */
+@WebServlet(name = "memberFormServlet", urlPatterns = "/servlet/members/new-form")
+public class MemberFormServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        response.setContentType("text/html");
+        response.setCharacterEncoding("utf-8");
+
+        PrintWriter w = response.getWriter();
+        w.write("<!DOCTYPE html>\n" +
+                "<html>\n" +
+                "<head>\n" +
+                " <meta charset=\"UTF-8\">\n" +
+                " <title>Title</title>\n" +
+                "</head>\n" +
+                "<body>\n" +
+                "<form action=\"/servlet/members/save\" method=\"post\">\n" +
+                " username: <input type=\"text\" name=\"username\" />\n" +
+                " age: <input type=\"text\" name=\"age\" />\n" +
+                " <button type=\"submit\">전송</button>\n" +
+                "</form>\n" +
+                "</body>\n" +
+                "</html>\n");
+    }
+}
